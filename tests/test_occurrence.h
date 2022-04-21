2580a23af115a7382e27433700b43cd6a8de33ce
Andrew Mary Huet de Barochez <phoqueeberlue@gmail.com>
2022-04-21 10:51:00 +0200

    Ajout de tests

--- tests/test_occurrence.h
+++ tests/test_occurrence.h
@@ -1,8 +1,21 @@
 //
 // Created by andrewmhdb on 19/04/2022.
 //
+#include "../src/occurrence.h"
 
-#ifndef CODAGE_HUFFMAN_OCCURRENCE_TEST_H
-#define CODAGE_HUFFMAN_OCCURRENCE_TEST_H
+#ifndef CODAGE_HUFFMAN_TEST_OCCURRENCE_H
+#define CODAGE_HUFFMAN_TEST_OCCURRENCE_H
 
-#endif //CODAGE_HUFFMAN_OCCURRENCE_TEST_H
+void testGetCharNodeByCharacter();
+
+void testGenerateOccurrences();
+
+void testSortCharListByOcc();
+
+void testSortCharListByASCIICode();
+
+void testFreeCharList();
+
+void testAllOccurrences();
+
+#endif //CODAGE_HUFFMAN_TEST_OCCURRENCE_H
